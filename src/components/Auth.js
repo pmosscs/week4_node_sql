6fb6946c9d3deca70d167d2be6a2c32095f955ee
Peyton Moss <pmoss94@gmail.com>
2022-12-05 21:16:40 -0700

    register functionality added

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,25 +1,80 @@
-import { useState } from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import AuthContext from "../store/authContext";
 
 const Auth = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [register, setRegister] = useState(true);
 
+  const authCtx = useContext(AuthContext);
+
   const submitHandler = (e) => {
     e.preventDefault();
+    const body = { username, password };
 
+    //transform into turnery to test things out:
+    // axios.post(register ? `${url}/register` : `${url}/login`, body)
+    //     .then(({data}) => {
+    //         console.log('AFTER AUTH', data)
+    //     })
+    //     .catch(err => {
+    //         setPassword('')
+    //         setUsername('')
+    //     })
+
+    if (register === true) {
+      axios
+        .post(`${baseURL}/register`, body)
+        .then((res) => {
+          console.log(res.data);
+          authCtx.login(res.data.token, res.data.exp, res.data.userId); //getting error "canot read properties of undefined (reading 'login')"
+        })
+        .catch((err) => console.log("register error", err));
+    } else {
+      axios
+        .post(`${baseURL}/login`, body)
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log("login error", err));
+    }
     console.log("submitHandler called");
   };
+
+  const baseURL = `https://socialmtn.devmountain.com`;
 
   return (
     <main>
       <h1>Welcome!</h1>
       <form className="form auth-form" onSubmit={submitHandler}>
-        <input className="form-input" />
-        <input className="form-input" />
-        <button className="form-btn">{register ? "Sign Up" : "Login"}</button>
+        <input
+          className="form-input"
+          type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          className="form-input"
+          type="text"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button className="form-btn" type="submit">
+          {register ? "Sign Up" : "Login"}
+        </button>
       </form>
-      <button className="form-btn">
+      <button
+        className="form-btn"
+        onClick={(e) => {
+          setRegister((current) => !current);
+          console.log(register);
+        }}
+      >
         Need to {register ? "Login" : "Sign Up"}?
       </button>
     </main>
--- src/store/authContext.js
+++ src/store/authContext.js
@@ -1,70 +1,78 @@
-import { useState, useEffect, useCallback, createContext } from 'react'
+import { useState, useEffect, useCallback, createContext } from "react";
 
-let logoutTimer
+let logoutTimer;
 
 const AuthContext = createContext({
-  token: '',
+  token: "",
   login: () => {},
   logout: () => {},
-  userId: null
-})
+  userId: null,
+});
 
 const calculateRemainingTime = (exp) => {
-  const currentTime = new Date().getTime()
-  const expTime = exp 
-  const remainingTime = expTime - currentTime
-  return remainingTime
-}
+  const currentTime = new Date().getTime();
+  const expTime = exp;
+  const remainingTime = expTime - currentTime;
+  return remainingTime;
+};
 
 const getLocalData = () => {
-  const storedToken = localStorage.getItem('token')
-  const storedExp = localStorage.getItem('exp')
+  const storedToken = localStorage.getItem("token");
+  const storedExp = localStorage.getItem("exp");
 
-  const remainingTime = calculateRemainingTime(storedExp)
+  const remainingTime = calculateRemainingTime(storedExp);
 
   if (remainingTime <= 1000 * 60 * 30) {
-    localStorage.removeItem('token')
-    localStorage.removeItem('exp')
-    return null
+    localStorage.removeItem("token");
+    localStorage.removeItem("exp");
+    return null;
   }
-
 
   return {
     token: storedToken,
     duration: remainingTime,
-  }
-}
-
-
+  };
+};
 
 export const AuthContextProvider = (props) => {
-  const localData = getLocalData()
-  
-  let initialToken
+  const localData = getLocalData();
+
+  let initialToken;
   if (localData) {
-    initialToken = localData.token
+    initialToken = localData.token;
   }
 
-  const [token, setToken] = useState(initialToken)
-  const [userId, setUserId] = useState(null)
+  const [token, setToken] = useState(initialToken);
+  const [userId, setUserId] = useState(null);
 
+  const logout = () => {};
 
-  const logout = () => {}
+  const login = (token, exp, userId) => {
+    console.log("login function being called");
+    setToken(token);
+    setUserId(userId);
 
-  const login = () => {}
+    localStorage.setItem("token", token);
+    localStorage.setItem("exp", exp);
+    localStorage.setItem("userID", userId);
+
+    const remainingTime = calculateRemainingTime(exp);
+
+    logoutTimer = setTimeout(logout, remainingTime);
+  };
 
   const contextValue = {
     token,
     login,
-    logout, 
-    userId
-  }
+    logout,
+    userId,
+  };
 
   return (
     <AuthContext.Provider value={contextValue}>
       {props.children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthContext
+export default { AuthContext };
